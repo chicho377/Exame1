ac93abce39f155de80cfcc820d68a435cfaf2917
chicho377 <morafabricio86@gmail.com>
2024-06-13 02:20:47 -0600

    Agregando bibliotecas y variables

---
+++ Codigo/GestionDeReciclaje.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, char** argv) {
+	
+	// variables
+	
+	// usuarios predeterminados
+	char nombreFabricio[30] = "Fabricio";
+    char nombreAlexander[30] = "Alexander";
+	
+	// materiales por usuario
+    int papelFabricio = 0, plasticoFabricio = 0, vidrioFabricio = 0, metalFabricio = 0, puntosFabricio = 0;
+    int papelAlexander = 0, plasticoAlexander = 0, vidrioAlexander = 0, metalAlexander = 0, puntosAlexander = 0;
+     
+	int opcion;
+    char nombreUsuario[30];
+    char material[30];
+    int cantidad;
+
+    int esFabricio = 0;
+    int esAlexander = 0;
+    
+    
+	return 0;
+}
